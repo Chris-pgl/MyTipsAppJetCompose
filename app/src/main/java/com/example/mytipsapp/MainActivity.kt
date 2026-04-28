5e47a39ce7e4a96f51333cb44b294dbd68bae1ae
Chris <cr1s@hotmail.it>
2026-04-28 15:33:14 +0200

    creating topHeader

--- app/src/main/java/com/example/mytipsapp/MainActivity.kt
+++ app/src/main/java/com/example/mytipsapp/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.mytipsapp
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.displayCutoutPadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mytipsapp.ui.theme.MyTipsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +36,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyTipsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(){
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+
+    }
+}
+@Preview
+@Composable
+fun TopHeader(){
+    Scaffold(modifier = Modifier.padding(6.dp)
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(RoundedCornerShape(12.dp))
+        .background(Color.LightGray)) {
+        Column(modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+            Text("Total Tip For Person")
+            Text("€134")
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyTipsAppTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
